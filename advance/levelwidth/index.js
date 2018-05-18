81c2c90142da3928220d2de5c809c1485b7d01d3
vladovoev <vladovoev@yahoo.co.uk>
2018-05-18 15:30:24 +0300

    level width

---
+++ advance/levelwidth/index.js
@@ -0,0 +1,44 @@
+// --- Directions
+// Given the root node of a tree, return
+// an array where each element is the width
+// of the tree at each level.
+// --- Example
+// Given:
+//     0
+//   / |  \
+// 1   2   3
+// |       |
+// 4       5
+// Answer: [1, 3, 2]
+
+const Node = require('./node');
+
+
+function levelWidth(root) {
+    const counterArr = [0];
+    const arr = [root, 's'];
+
+    while (arr.length > 1) {
+        const node = arr.shift();
+
+        if (node === 's') {
+            counterArr.push(0);
+            arr.push('s');
+        } else {
+            arr.push(...node.children);
+            counterArr[counterArr.length - 1]++;
+        }
+    }
+
+    return counterArr;
+}
+
+const root = new Node(0);
+root.add(1);
+root.children[0].add(2);
+root.children[0].add(3);
+root.children[0].children[0].add(4);
+console.log(levelWidth(root));
+
+
+module.exports = levelWidth;
