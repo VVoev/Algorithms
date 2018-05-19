4bff67511b30d24157925c51c6f6ee8b9b4ffec4
vladovoev <vladovoev@yahoo.co.uk>
2018-05-19 19:19:33 +0300

    selection sort

--- advance/sortingAlgo/index.js
+++ advance/sortingAlgo/index.js
@@ -15,7 +15,16 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
-
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
 }
 
 function mergeSort(arr) {
@@ -25,5 +34,9 @@ function mergeSort(arr) {
 function merge(left, right) {
 
 }
+
+let arr = [100, -40, 500, -124, 0, 21, 7];
+let sorted = selectionSort(arr);
+console.log(sorted);
 
 module.exports = { bubbleSort, selectionSort, mergeSort };
