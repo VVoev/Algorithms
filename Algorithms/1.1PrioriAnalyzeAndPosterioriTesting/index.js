cafc43668eef9e6084e9ea96a01e63c938dc1acb
vladovoev <vladovoev@yahoo.co.uk>
2018-05-21 16:21:08 +0300

    Priori Analize and Posteriori Testing

---
+++ Algorithms/1.1PrioriAnalyzeAndPosterioriTesting/index.js
@@ -0,0 +1,7 @@
+/*
+    Priori Analysis                         Posteriori Testing
+    1.Algorithm                             1.Program
+    2.Independent & Language                2.Language Dependent
+    3.Hardware Independent                  3.Hardware dependent
+    4.Time & Space function                 4.watch time & bytes
+*/
