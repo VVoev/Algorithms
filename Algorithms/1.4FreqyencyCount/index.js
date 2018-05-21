acc05f7c3228a68cd28f28f7512df2aa7a464470
vladovoev <vladovoev@yahoo.co.uk>
2018-05-21 17:03:09 +0300

    FreqyencyCount

---
+++ Algorithms/1.4FreqyencyCount/index.js
@@ -0,0 +1,29 @@
+/*
+        Freqyency Count
+        Sum(a,n)
+        {
+            s=0;                        >1
+            for(i=0;i<n;i++)            >n+1
+            {
+                s=s+a[i]                >n
+            }
+            return s;                   >1
+        }                           ------------
+                                        f(n)=2n+3
+                                        O(n)
+                                        s(n)=n+3
+                                        O(n)
+
+
+        add(a,b,n)
+        {
+            for(i=0;i<n;i++)
+            {
+                for(j=0;j<n;j++)
+                {
+                    c[i,j] = a[i,j] + b[i,j];
+                }
+            }
+        }
+
+*/
