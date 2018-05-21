636848f88355d71f93eb053228d5af574830565a
vladovoev <vladovoev@yahoo.co.uk>
2018-05-21 16:17:45 +0300

    intro

---
+++ Algorithms/1.IntroToAlgo/index.js
@@ -0,0 +1,14 @@
+/*
+
+                    Algorithms
+
+Algorithm                               Program
+Design time                             Implementation
+Domain Knowledge                        programmer
+any language                            programming Lang
+H/W & OS                                H/W & OS
+Analyze                                 Testing
+
+
+
+*/
