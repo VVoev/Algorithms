1390d16a030f3b65e178b1dd69831e8a7686116a
vladovoev <vladovoev@yahoo.co.uk>
2018-05-22 16:42:49 +0300

    compare functions

---
+++ Algorithms/1.6ClassesOfFunctions/index.js
@@ -0,0 +1,15 @@
+/*
+    types of time functions
+
+    f(n)=2
+    f(n)=5      => constant
+    f(n)=5000
+
+    O(i)                    =>  Constant
+    O(logn)                 =>  LogN
+    O(n)                    =>  Linear
+    O(nSquare)              =>  Square
+    O(nCubic)               =>  Cubic
+    O(2N)Exponential        =>  Exponential
+
+*/
